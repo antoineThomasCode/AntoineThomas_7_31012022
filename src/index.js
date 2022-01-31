e6ffa98211471aecac226b98f71df5794f2f97e0
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-01-31 12:56:22 +0100

    create init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import recipes from "./data/recipes";
+import initHomepage from "./pages/homepage";
+
+initHomepage(recipes)
---
+++ src/pages/homepage.js
@@ -0,0 +1,10 @@
+
+
+const initHomepage = (array) => {
+    const idArray = []
+    array.forEach(recipe => {
+        console.log(recipe.name)
+    });
+    console.log(idArray)
+}
+export default initHomepage
