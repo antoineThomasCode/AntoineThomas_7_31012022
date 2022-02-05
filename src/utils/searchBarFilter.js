df9cbf3bfa438582a0eb708607796df10ab0be8f
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-02-05 17:03:18 +0100

    search bar using filter method

--- src/utils/searchBarFilter.js
+++ src/utils/searchBarFilter.js
@@ -1,29 +1,13 @@
-import recipes from "../data/recipes";
+
 import { searchBar } from "../pages/homepage";
 
 function filterbySearchBar(recipes) {
-    const filteredArray = []
+    const filteredArray = recipes.filter((recipe) => { return (
+        recipe.name.toLowerCase().includes(searchBar.value.toLowerCase()) || 
+        recipe.description.toLowerCase().includes(searchBar.value.toLowerCase()) ||
+        recipe.ingredients.includes((ingredient) => ingredient.ingredient.toLowerCase().includes(searchBar.value.toLowerCase())))
+    })
     
-    for (let i = recipes.length; i > 0; i--) {
-        let containsName = false
-        let containsDescription = false 
-        let containsIngredients = false 
-        if (recipes[i]?.name.toLowerCase().includes(searchBar.value.toLowerCase())) {
-            
-            containsName = true 
-        }
-        if (recipes[i]?.description.toLowerCase().includes(searchBar.value.toLowerCase())) {
-            containsDescription = true 
-        }
-        for (let y = 0; y < recipes[i]?.ingredients[y]?.ingredient.length; y++) {
-            if (recipes[i].ingredients[y]?.ingredient?.toLowerCase().includes(searchBar.value.toLowerCase())){
-              containsIngredients = true;
-            }
-          }
-        if (containsName || containsDescription || containsIngredients){
-            filteredArray.push(recipes[i])
-        }
-    }
     return filteredArray
 }
 export default filterbySearchBar
